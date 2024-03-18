492b3b7474e9e23b35663d7c86f6077c4cb24bb7
Yam <buithanhdan2005@gmail.com>
2024-03-18 17:00:24 +0700

    vie to eng

--- DictionaryApp/src/main/java/com/example/dictionaryapp/Controller.java
+++ DictionaryApp/src/main/java/com/example/dictionaryapp/Controller.java
@@ -1,12 +1,14 @@
 package com.example.dictionaryapp;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
@@ -21,6 +23,10 @@ import javafx.scene.media.MediaPlayer;
 
 
 public class Controller {
+    @FXML
+    private Label myLabel;
+    @FXML
+    private CheckBox choice;
 
     @FXML
     private ListView<String> listView;
@@ -33,10 +39,13 @@ public class Controller {
     public void exitProgram() {
         System.exit(0);
     }
-    public  Map<String,String> data = Main.getData();
+
     public Set<String> set = data.keySet();
-    public void search() {
+    public static boolean first_time = true;
+    public void search() throws IOException {
+        if(first_time) readData();
         String text = searchWord.getText();
+        //String lowerCasetext = text.toLowerCase();
 
             if (set.contains(text)) {
                 definitionView.getEngine().loadContent(data.get(text), "text/html");
@@ -71,7 +80,7 @@ public class Controller {
     }
     private static String myKey = "40ccd1f320c549f3afc53b26046c49a4";
     public static String ACCENT = Languages.English_GreatBritain;
-    private String path = "data/tts_rss_word.mp3";
+    public static String path = "data/tts_rss_word.mp3";
     public void requestDownload(String text) throws Exception {
         VoiceProvider tts = new VoiceProvider(myKey);
 
@@ -96,5 +105,65 @@ public class Controller {
         Media sound = new Media(new File(path).toURI().toString());
         MediaPlayer mediaPlayer = new MediaPlayer(sound);
         mediaPlayer.play();
+    }
+    //public static boolean checkBox = true;
+    public void change() throws IOException {
+        if(choice.isSelected()) {
+            first_time = false;
+            myLabel.setText("Vie-Eng");
+            path = "data/tts_rss_text.mp3";
+            ACCENT = Languages.Vietnamese;
+            FILE_PATH = "data/vie_eng.txt";
+            data.clear();
+            set.clear();
+            readData();
+
+        } else {
+            myLabel.setText("Eng-Vie");
+            path = "data/tts_rss_word.mp3";
+            ACCENT = Languages.English_GreatBritain;
+            FILE_PATH = "data/eng_vie.txt";
+            readData();
+        }
+    }
+
+    class Word {
+        private String word;
+        private String def;
+
+        public Word(String word, String def) {
+            this.word = word;
+            this.def = def;
+        }
+
+        public String getWord() {
+            return word;
+        }
+
+        public void setWord(String word) {
+            this.word = word;
+        }
+
+        public String getDef() {
+            return def;
+        }
+
+        public void setDef(String def) {
+            this.def = def;
+        }
+    }
+    public static String FILE_PATH = "data/eng_vie.txt";
+    public static Map<String, String> data = new HashMap<>();
+    public void readData() throws IOException {
+        FileReader fis = new FileReader(FILE_PATH);
+        BufferedReader br = new BufferedReader(fis);
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] parts = line.split("<html>");
+            String word = parts[0];
+            String definition = "<html>" + parts[1];
+            data.put(word, definition);
+        }
+
     }
 }
--- DictionaryApp/src/main/java/com/example/dictionaryapp/Main.java
+++ DictionaryApp/src/main/java/com/example/dictionaryapp/Main.java
@@ -13,56 +13,10 @@ import java.util.Map;
 
 public class Main extends Application {
 
-    class Word {
-        private String word;
-        private String def;
-
-        public Word(String word, String def) {
-            this.word = word;
-            this.def = def;
-        }
-
-        public String getWord() {
-            return word;
-        }
-
-        public void setWord(String word) {
-            this.word = word;
-        }
-
-        public String getDef() {
-            return def;
-        }
-
-        public void setDef(String def) {
-            this.def = def;
-        }
-    }
-
-    private static final String FILE_PATH = "data/eng_vie.txt";
-    public static Map<String, String> data = new HashMap<>();
-    public void readData() throws IOException {
-        FileReader fis = new FileReader(FILE_PATH);
-        BufferedReader br = new BufferedReader(fis);
-        String line;
-        while ((line = br.readLine()) != null) {
-            String[] parts = line.split("<html>");
-            String word = parts[0];
-            String definition = "<html>" + parts[1];
-            data.put(word, definition);
-        }
-
-    }
-
-    public static Map<String, String> getData() {
-        return data;
-    }
-
     @Override
     public void start(Stage stage) throws IOException {
-        readData();
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1300, 700);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 500);
         stage.setTitle("DictionaryApp");
         stage.setScene(scene);
         stage.show();
