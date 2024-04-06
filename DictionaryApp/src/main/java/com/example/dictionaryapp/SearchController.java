a89226c083813a8302dc31c56f2a5147318ffd68
Yam <buithanhdan2005@gmail.com>
2024-04-06 23:04:28 +0700

    add something new

---
+++ DictionaryApp/src/main/java/com/example/dictionaryapp/SearchController.java
@@ -0,0 +1,184 @@
+package com.example.dictionaryapp;
+
+import com.voicerss.tts.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.web.WebView;
+import java.io.*;
+import java.net.URL;
+import java.util.*;
+
+public class SearchController extends MainController implements Initializable {
+
+    private static final String myKey = "40ccd1f320c549f3afc53b26046c49a4";
+    @FXML
+    protected ListView<String> listView = new ListView<>();
+    @FXML
+    protected WebView definitionView;
+    @FXML
+    protected TextField searchWord;
+    @FXML
+    protected ImageView imageView;
+    @FXML
+    protected ImageView favorite;
+    @FXML
+    protected Label label;
+    Image pic1 = new Image(getClass().getResourceAsStream("images/eng-viet.png"));
+    Image pic2 = new Image(getClass().getResourceAsStream("images/viet-eng.png"));
+    Image pic3 = new Image(getClass().getResourceAsStream("images/icons8_Star_Filled_52px.png"));
+    Image pic4 = new Image(getClass().getResourceAsStream("images/icons8_Star_52px.png"));
+
+    public static boolean setLanguage = false;
+    public static boolean setFavorite = false;
+    public static Map<String, String> data = data_eng2vie;
+    public static Set<String> set = set_eng2vie;
+    public static List<String> curr;
+    public static Map<String, List<String>> prefixMap = Map_eng2vie;
+    public static String ACCENT = Languages.English_UnitedStates;
+    public static String path;
+
+   /* public void keyPressed(KeyEvent e) throws Exception {
+        if (e.getCode().equals(KeyCode.ENTER)) {
+            search();
+        }
+    }*/
+
+    public void search() throws Exception {
+        String text = searchWord.getText();
+        //String lowerCase-text = text.toLowerCase();
+        if (set.contains(text)) {
+            definitionView.getEngine().loadContent(data.get(text), "text/html");
+            requestDownload(text);
+        }
+    }
+
+    public void selectedWord() throws Exception {
+        String selected = listView.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            definitionView.getEngine().loadContent(data.get(selected), "text/html");
+            searchWord.setText(selected);
+            requestDownload(selected);
+        }
+    }
+
+    public void changeLanguage() {
+        if (setLanguage) {
+            imageView.setImage(pic2);
+            setLanguage = false;
+            prefixMap = Map_vie2eng;
+            set = set_vie2eng;
+            data = data_vie2eng;
+            path = "data/Voice1.mp3";
+            ACCENT = Languages.Vietnamese;
+            label.setText("VIE");
+            //savedPath = "data/VE_saved.txt";
+        } else {
+            imageView.setImage(pic1);
+            setLanguage = true;
+            prefixMap = Map_eng2vie;
+            set = set_eng2vie;
+            data = data_eng2vie;
+            path = "data/Voice2.mp3";
+            ACCENT = Languages.English_UnitedStates;
+            label.setText("US");
+            //savedPath = "data/VE_saved.txt";
+        }
+    }
+
+    public void changeLanguageForSetting(boolean setLang) {
+        if (setLang) {
+            setLang = false;
+            prefixMap = Map_vie2eng;
+            set = set_vie2eng;
+            data = data_vie2eng;
+        } else {
+            setLang = true;
+            prefixMap = Map_eng2vie;
+            set = set_eng2vie;
+            data = data_eng2vie;
+        }
+    }
+    public static boolean setUS_UK = false;
+    public void US_UK() {
+        if (setUS_UK) {
+            label.setText("UK");
+            ACCENT = Languages.English_GreatBritain;
+            setUS_UK = false;
+        } else {
+            label.setText("US");
+            ACCENT = Languages.English_UnitedStates;
+            setUS_UK = true;
+        }
+    }
+
+    public void showAllWords() {
+        String text = searchWord.getText();
+
+        if (text.length() == 1) {
+            listView.getItems().removeAll(set);
+            curr = prefixMap.getOrDefault(text, new ArrayList<>());
+            listView.getItems().addAll(curr);
+
+        } else if (text.length() > 1) {
+            listView.getItems().removeAll(curr);
+            curr = prefixMap.getOrDefault(text, new ArrayList<>());
+            listView.getItems().addAll(curr);
+        }
+    }
+
+
+    public void requestDownload(String text) throws Exception {
+        VoiceProvider tts = new VoiceProvider(myKey);
+        VoiceParameters params = new VoiceParameters(text, ACCENT);
+        params.setCodec(AudioCodec.MP3);
+        params.setFormat(AudioFormat.Format_44KHZ.AF_44khz_16bit_stereo);
+        params.setBase64(false);
+        params.setSSML(false);
+        params.setRate(0);
+        byte[] voice = tts.speech(params);
+        FileOutputStream fos = new FileOutputStream(path);
+        fos.write(voice, 0, voice.length);
+        fos.flush();
+        fos.close();
+    }
+
+    public void spelling() {
+        Media sound = new Media(new File(path).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
+        mediaPlayer.play();
+    }
+
+    public void favorite_click() {
+        if (setFavorite) {
+            favorite.setImage(pic3);
+            setFavorite = false;
+        } else {
+            favorite.setImage(pic4);
+            setFavorite = true;
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setLanguage = true;
+        prefixMap = Map_eng2vie;
+        set = set_eng2vie;
+        data = data_eng2vie;
+        path = "data/Voice2.mp3";
+        ACCENT = Languages.English_UnitedStates;
+        label.setText("US");
+        listView.getItems().addAll(set);
+        //savedPath = "data/EV_saved.txt";
+        //listWords = list_EV;
+    }
+
+
+
+}
---
+++ DictionaryApp/src/main/java/com/example/dictionaryapp/SettingController.java
@@ -0,0 +1,236 @@
+package com.example.dictionaryapp;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.web.HTMLEditor;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class SettingController extends SearchController implements Initializable {
+
+    @FXML
+    private TextField searchWord;
+    @FXML
+    private ListView<String> listView;
+    @FXML
+    private HTMLEditor htmlEditor;
+    @FXML
+    private RadioButton english;
+    @FXML
+    private RadioButton vietnamese;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextArea getSubject;
+    @FXML
+    private TextArea getContent;
+    private  String pathRes;
+    public static List<String> listWords;
+
+    public void search() throws Exception {
+        String text = searchWord.getText();
+        //String lowerCase-text = text.toLowerCase();
+        if (set.contains(text)) {
+            htmlEditor.setHtmlText(data.get(text));
+        }
+    }
+
+    public void selectedWord() throws Exception {
+        String selected = listView.getSelectionModel().getSelectedItem();
+        if (selected != null) {
+            htmlEditor.setHtmlText(data.get(selected));
+            searchWord.setText(selected);
+
+        }
+    }
+
+    public void showAddWord() {
+        htmlEditor.setHtmlText("<html>" + searchWord.getText() + "<br/><ul><li><b><i> Part of speech: " +
+                "</i></b><ul><li><font color='#233fdb'><b> Definition: " +
+                "</b></font></li></ul></li></ul></html>");
+    }
+
+    public void setListEV() {
+        vietnamese.setSelected(false);
+        english.setSelected(true);
+        listWords = list_EV;
+        data = data_eng2vie;
+        pathRes = "data/EV.txt";
+        changeLanguageForSetting(false);
+    }
+
+    public void setListVE() {
+        english.setSelected(false);
+        vietnamese.setSelected(true);
+        listWords = list_VE;
+        data = data_vie2eng;
+        pathRes = "data/VE.txt";
+        changeLanguageForSetting(true);
+
+    }
+
+
+    public void buttonDelete() throws IOException {
+        String word = searchWord.getText();
+        String definition = data.get(word);
+        ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+        ButtonType no = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to delete this word?", yes, no);
+        alert.setTitle("Notification");
+        alert.setHeaderText(null);
+        alert.showAndWait();
+        if (alert.getResult() == yes) {
+            FileWriter fw = new FileWriter(pathRes);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (String content : listWords) {
+                if (!content.equals(word + definition)) {
+                    bw.write(content);
+                    bw.write("\n");
+                }
+            }
+            bw.close();
+            fw.close();
+            Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+            alert1.setTitle("Notification");
+            alert1.setHeaderText(null);
+            alert1.setContentText("Deleted successfully!");
+            alert1.showAndWait();
+        }
+    }
+
+    public void buttonSave() throws IOException {
+        String word = searchWord.getText();
+        if (set.contains(word)) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Notification");
+            alert.setHeaderText(null);
+            alert.setContentText("This word is existed!");
+            alert.showAndWait();
+        } else {
+            String definition = htmlEditor.getHtmlText().replace(" dir=\"ltr\"", "");
+            String text = word + definition;
+            listWords.addLast(text);
+            FileWriter fw = new FileWriter(pathRes);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (String content : listWords) {
+                bw.write(content);
+                bw.write("\n");
+            }
+            bw.close();
+            fw.close();
+            Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+            alert1.setTitle("Notification");
+            alert1.setHeaderText(null);
+            alert1.setContentText("Added successfully!");
+            alert1.showAndWait();
+        }
+    }
+
+    public void buttonModify() throws IOException {
+        ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+        ButtonType no = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Do you want to modify this word?", yes, no);
+        alert.setTitle("Notification");
+        alert.setHeaderText(null);
+        alert.showAndWait();
+        if (alert.getResult() == yes) {
+            String word = searchWord.getText();
+            String beforeDefinition = data.get(word);
+            String afterDefinition = htmlEditor.getHtmlText().replace(" dir=\"ltr\"", "");
+            FileWriter fw = new FileWriter(pathRes);
+            BufferedWriter bw = new BufferedWriter(fw);
+            for (String content : listWords) {
+                if (!content.equals(word + beforeDefinition)) {
+                    bw.write(content);
+                    bw.write("\n");
+                } else {
+                    bw.write(word + afterDefinition);
+                    bw.write("\n");
+                }
+            }
+            bw.close();
+            fw.close();
+            Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+            alert1.setTitle("Notification");
+            alert1.setHeaderText(null);
+            alert1.setContentText("Modified successfully!");
+            alert1.showAndWait();
+        }
+    }
+
+    private static final String from = "java.application.from@gmail.com";
+    private static final String to = "java.application.to@gmail.com";
+    private static final String password = "rnrraicahugtfbbw";
+
+    public static boolean sendEmail(String subject, String content) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587"); // TLS 587 SSL 465
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
+
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                // TODO Auto-generated method stub
+                return new PasswordAuthentication(from, password);
+            }
+        };
+        Session session = Session.getInstance(props, auth);
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+
+            //msg.setFrom(from);
+
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+
+            msg.setSubject(subject);
+
+            msg.setSentDate(new Date());
+
+            msg.setContent(content, "text/HTML; charset=UTF-8");
+
+            Transport.send(msg);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public void send() {
+        String subject = getSubject.getText();
+        String content = email.getText() + "\n" + getContent.getText();
+        SettingController.sendEmail(subject, content);
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Notification");
+        alert.setHeaderText(null);
+        alert.setContentText("Sent successfully!");
+        alert.showAndWait();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        pathRes = "data/EV.txt";
+        listWords = list_EV;
+        data = data_eng2vie;
+        english.setSelected(true);
+    }
+}
